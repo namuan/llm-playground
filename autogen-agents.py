d3b7b855c7990a28aa886ef2bd80e701c4faf16b
namuan <575441+namuan@users.noreply.github.com>
2023-10-22 17:49:59 +0100

    feat: Simple example using autogen framework

---
+++ autogen-agents.py
@@ -0,0 +1,33 @@
+import random
+
+import autogen
+
+# config_list = [{"model": "gpt-4", "api_base": "http://0.0.0.0:8000", "api_key": "NULL"}]
+config_list = [{"model": "gpt-4"}]
+
+seed = random.randint(0, 1000000)
+print(f"seed: {seed}")
+
+assistant = autogen.AssistantAgent(
+    name="Assistant",
+    llm_config={
+        "seed": seed,
+        "config_list": config_list,
+        "temperature": 0,
+    },
+)
+
+user_proxy = autogen.UserProxyAgent(
+    name="UserProxy",
+    human_input_mode="ALWAYS",
+    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+    code_execution_config={"work_dir": "target", "use_docker": False},
+)
+
+user_proxy.initiate_chat(
+    assistant,
+    message="""
+    Create a Plot to show the percent gain of MSFT, AAPL, TSLA, NVDA, NFLX, and GOOG stocks for each day in the year-to-date period
+    And save it in a file called "stocks-gains.png"
+    """,
+)
