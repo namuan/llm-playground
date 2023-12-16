6332a7f4b8cfdcd8dcd6bef96235d0a4db0561ea
namuan <575441+namuan@users.noreply.github.com>
2023-12-16 11:50:15 +0000

    feat: Upgrade APIs

--- openai-tools-simple.py
+++ openai-tools-simple.py
@@ -4,19 +4,19 @@ Playing with OpenAI tools and function
 https://cookbook.openai.com/examples/how_to_call_functions_with_chat_models
 """
 import logging
-import os
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
-import openai
-import requests
 from dotenv import load_dotenv
+from openai import OpenAI
+from openai.types.chat import ChatCompletionMessage
 from tenacity import retry, stop_after_attempt, wait_random_exponential
 from termcolor import colored
 
 load_dotenv()
 
 GPT_MODEL = "gpt-3.5-turbo-1106"
-openai.api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI()
 
 
 def setup_logging(verbosity):
@@ -53,24 +53,12 @@ def parse_args():
 
 
 @retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
-def chat_completion_request(messages, tools=None, tool_choice=None, model=GPT_MODEL):
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + openai.api_key,
-    }
-    json_data = {"model": model, "messages": messages}
-    if tools is not None:
-        json_data.update({"tools": tools})
-    if tool_choice is not None:
-        json_data.update({"tool_choice": tool_choice})
-
+def chat_completion_request_api(messages, tools=None, model=GPT_MODEL):
     try:
-        response = requests.post(
-            "https://api.openai.com/v1/chat/completions",
-            headers=headers,
-            json=json_data,
+        response = client.chat.completions.create(
+            model=model, messages=messages, tools=tools
         )
-        return response
+        return response.choices[0].message
     except Exception as e:
         print("Unable to generate ChatCompletion response")
         print(f"Exception {e}")
@@ -86,34 +74,37 @@ def pretty_print_conversation(messages):
     }
 
     for message in messages:
-        if message["role"] == "system":
+        if isinstance(message, ChatCompletionMessage):
+            message_role = message.role
+            message_content = message.content
+            message_name = message.function_call.name if message.function_call else None
+            message_function_call = message.function_call
+        else:
+            message_role = message["role"]
+            message_content = message["content"]
+            message_name = message.get("name")
+            message_function_call = message.get("function_call")
+
+        if message_role == "system":
+            print(colored(f"system: {message_content}\n", role_to_color[message_role]))
+        elif message_role == "user":
+            print(colored(f"user: {message_content}\n", role_to_color[message_role]))
+        elif message_role == "assistant" and message_function_call:
             print(
                 colored(
-                    f"system: {message['content']}\n", role_to_color[message["role"]]
+                    f"assistant: {message_function_call}\n",
+                    role_to_color[message_role],
                 )
             )
-        elif message["role"] == "user":
+        elif message_role == "assistant" and not message_function_call:
             print(
-                colored(f"user: {message['content']}\n", role_to_color[message["role"]])
+                colored(f"assistant: {message_content}\n", role_to_color[message_role])
             )
-        elif message["role"] == "assistant" and message.get("function_call"):
+        elif message_role == "tool":
             print(
                 colored(
-                    f"assistant: {message['function_call']}\n",
-                    role_to_color[message["role"]],
-                )
-            )
-        elif message["role"] == "assistant" and not message.get("function_call"):
-            print(
-                colored(
-                    f"assistant: {message['content']}\n", role_to_color[message["role"]]
-                )
-            )
-        elif message["role"] == "tool":
-            print(
-                colored(
-                    f"function ({message['name']}): {message['content']}\n",
-                    role_to_color[message["role"]],
+                    f"function ({message_name}): {message_content}\n",
+                    role_to_color[message_role],
                 )
             )
 
@@ -187,11 +178,10 @@ def main(args):
             "content": "what is the weather going to be like in London, UK and Dubai over the next 5 days",
         }
     )
-    chat_response = chat_completion_request(
+    assistant_message = chat_completion_request_api(
         messages,
         tools=tools,
     )
-    assistant_message = chat_response.json()["choices"][0]["message"]
     messages.append(assistant_message)
     print(assistant_message)
     pretty_print_conversation(messages)
