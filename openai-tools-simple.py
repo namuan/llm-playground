70bbacc43e07d4503cd6074eade0e4e3a0f61150
namuan <575441+namuan@users.noreply.github.com>
2023-12-16 09:30:14 +0000

    feat: Learning OpenAI api to call functions

---
+++ openai-tools-simple.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python3
+"""
+Playing with OpenAI tools and function
+https://cookbook.openai.com/examples/how_to_call_functions_with_chat_models
+"""
+import logging
+import os
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+
+import openai
+import requests
+from dotenv import load_dotenv
+from tenacity import retry, stop_after_attempt, wait_random_exponential
+from termcolor import colored
+
+load_dotenv()
+
+GPT_MODEL = "gpt-3.5-turbo-1106"
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+def setup_logging(verbosity):
+    logging_level = logging.WARNING
+    if verbosity == 1:
+        logging_level = logging.INFO
+    elif verbosity >= 2:
+        logging_level = logging.DEBUG
+
+    logging.basicConfig(
+        handlers=[
+            logging.StreamHandler(),
+        ],
+        format="%(asctime)s - %(filename)s:%(lineno)d - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging_level,
+    )
+    logging.captureWarnings(capture=True)
+
+
+def parse_args():
+    parser = ArgumentParser(
+        description=__doc__, formatter_class=RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        dest="verbose",
+        help="Increase verbosity of logging output",
+    )
+    return parser.parse_args()
+
+
+@retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
+def chat_completion_request(messages, tools=None, tool_choice=None, model=GPT_MODEL):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + openai.api_key,
+    }
+    json_data = {"model": model, "messages": messages}
+    if tools is not None:
+        json_data.update({"tools": tools})
+    if tool_choice is not None:
+        json_data.update({"tool_choice": tool_choice})
+
+    try:
+        response = requests.post(
+            "https://api.openai.com/v1/chat/completions",
+            headers=headers,
+            json=json_data,
+        )
+        return response
+    except Exception as e:
+        print("Unable to generate ChatCompletion response")
+        print(f"Exception {e}")
+        return e
+
+
+def pretty_print_conversation(messages):
+    role_to_color = {
+        "system": "red",
+        "user": "green",
+        "assistant": "blue",
+        "tool": "magenta",
+    }
+
+    for message in messages:
+        if message["role"] == "system":
+            print(
+                colored(
+                    f"system: {message['content']}\n", role_to_color[message["role"]]
+                )
+            )
+        elif message["role"] == "user":
+            print(
+                colored(f"user: {message['content']}\n", role_to_color[message["role"]])
+            )
+        elif message["role"] == "assistant" and message.get("function_call"):
+            print(
+                colored(
+                    f"assistant: {message['function_call']}\n",
+                    role_to_color[message["role"]],
+                )
+            )
+        elif message["role"] == "assistant" and not message.get("function_call"):
+            print(
+                colored(
+                    f"assistant: {message['content']}\n", role_to_color[message["role"]]
+                )
+            )
+        elif message["role"] == "tool":
+            print(
+                colored(
+                    f"function ({message['name']}): {message['content']}\n",
+                    role_to_color[message["role"]],
+                )
+            )
+
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "get_current_weather",
+            "description": "Get the current weather",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "The city and state, e.g. San Francisco, CA",
+                    },
+                    "format": {
+                        "type": "string",
+                        "enum": ["celsius", "fahrenheit"],
+                        "description": "The temperature unit to use. Infer this from the users location.",
+                    },
+                },
+                "required": ["location", "format"],
+            },
+        },
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "get_n_day_weather_forecast",
+            "description": "Get an N-day weather forecast",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "The city and state, e.g. San Francisco, CA",
+                    },
+                    "format": {
+                        "type": "string",
+                        "enum": ["celsius", "fahrenheit"],
+                        "description": "The temperature unit to use. Infer this from the users location.",
+                    },
+                    "num_days": {
+                        "type": "integer",
+                        "description": "The number of days to forecast",
+                    },
+                },
+                "required": ["location", "format", "num_days"],
+            },
+        },
+    },
+]
+
+
+def main(args):
+    messages = []
+    messages.append(
+        {
+            "role": "system",
+            "content": "Don't make assumptions about what values to plug into functions. "
+            "Ask for clarification if a user request is ambiguous.",
+        }
+    )
+    # messages.append({"role": "user", "content": "What is the weather like today?"})
+    # messages.append({"role": "user", "content": "I'm in London, UK"})
+    messages.append(
+        {
+            "role": "user",
+            "content": "what is the weather going to be like in London, UK and Dubai over the next 5 days",
+        }
+    )
+    chat_response = chat_completion_request(
+        messages,
+        tools=tools,
+    )
+    assistant_message = chat_response.json()["choices"][0]["message"]
+    messages.append(assistant_message)
+    print(assistant_message)
+    pretty_print_conversation(messages)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    setup_logging(args.verbose)
+    main(args)
