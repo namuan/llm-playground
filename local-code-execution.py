eb54fa6fdb0394710626987d4591a6d63b89d1fb
namuan <575441+namuan@users.noreply.github.com>
2025-01-13 12:32:05 +0000

    feat: Local code execution using ipybox

---
+++ local-code-execution.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env -S uv run --quiet --script
+# /// script
+# dependencies = [
+#   "ipybox==0.3.1",
+# ]
+# ///
+import asyncio
+
+from ipybox import ExecutionClient, ExecutionContainer
+
+
+async def main():
+    async with ExecutionContainer(tag="ghcr.io/gradion-ai/ipybox:minimal") as container:
+        async with ExecutionClient(port=container.port) as client:
+            result = await client.execute("print('Hello, world!')")
+            print(f"Output: {result.text}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
